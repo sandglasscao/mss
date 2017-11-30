09e970aa06af6dba9537ba5f75064eb14645cf0c
zhengwei2531 <1820468354@qq.com>
2017-11-30 11:07:51 +0800

    *ngFor show list change

--- static/src/app/ts/dashboard/stores.componet.ts
+++ static/src/app/ts/dashboard/stores.componet.ts
@@ -10,7 +10,55 @@ import {Router} from "@angular/router";
 export class StoresComponent implements OnInit {
   constructor(private router: Router) {
   }
-
+  teamMembers = [{
+    "id": "1",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  },{
+    "id": "2",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  },{
+    "id": "3",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  },{
+    "id": "4",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  },{
+    "id": "5",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  },{
+    "id": "6",
+    "img": "logo1.jpg",
+    "name": "公主殿下",
+    "old": 21,
+    "new": 7,
+    "order": 51,
+    "downline": 51,
+  }];
   ngOnInit(): void {
     let isLogin = sessionStorage.getItem('isLogin');
     if (!isLogin) {
--- static/src/app/ts/dashboard/team.component.ts
+++ static/src/app/ts/dashboard/team.component.ts
@@ -10,6 +10,21 @@ import {Router} from "@angular/router";
 export class TeamComponent implements OnInit {
   constructor(private router: Router) {
   }
+  items = [
+    "北京大兴区富康路188号",
+    "北京大兴区富康路187号",
+    "北京大兴区富康路158号",
+    "北京大兴区xinhua158号",
+    "北京海淀区唐家岭路356号",
+    "北京朝阳区唐新芳路79号",
+    "北京大兴区唐北兴路179号",
+    "北京海淀区唐家岭路35号",
+    "北京朝阳区唐新芳路752号",
+    "北京大兴区唐北兴路19号",
+    "北京海淀区唐家岭路56号",
+    "北京朝阳区唐新芳路70号",
+    "北京大兴区唐北兴路79号"
+  ];
   onActive = true;
   decide(tf: boolean) {
     this.onActive = tf;
