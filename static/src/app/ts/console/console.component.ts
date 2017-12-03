4fa3200862e8799e768f2df3651f56556c534a66
zhengwei2531 <1820468354@qq.com>
2017-12-03 19:19:38 +0800

    console css change;

--- static/src/app/ts/console/console.component.ts
+++ static/src/app/ts/console/console.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  templateUrl: 'static/src/app/templates/console/console.html'
+  templateUrl: 'static/src/app/templates/console/console.html',
+  styleUrls: ['static/src/app/templates/console/console.css']
 })
 export class ConsoleComponent{
 }
