f05875f966f407af6083d313aea900c05db7b3c6
mengruiqia <mrqia@outlook.com>
2017-12-05 13:57:05 +0800

    cellphone-reset.html+component.ts+service.ts(not complete)

--- static/src/app/ts/cellphone-reset.component.ts
+++ static/src/app/ts/cellphone-reset.component.ts
@@ -1,24 +1,60 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 
-import {RegisterService} from "./registrater.service";
 
-
+import {CellphoneResetService} from './cellphone-reset.service';
+import {User} from './dashboard/user';
+import {ProfileService} from './dashboard/profile.service';
 @Component({
   selector: 'cell-reset',
   templateUrl: 'static/src/app/templates/dashboard/cellphone-reset.html',
-  styleUrls: ['static/src/app/templates/dashboard/cellphone.css']
+  styleUrls: ['static/src/app/templates/dashboard/cellphone.css'],
+  providers: [
+    CellphoneResetService,
+  ]
 })
 
 
 export class CellPhoneResetComponent {
+  user = new User();
+  next = 'reset-pwd';
   cellPhone: string;
   smsCode: string;
   verification = null;
   error = null;
 
-  constructor(private registerService: RegisterService,
+  constructor(private cellphoneService: CellphoneResetService,
+              private profileService: ProfileService,
               private router: Router) {
+  }
+  onSub() {
+    if (this.user.username) {
+      this.cellphoneService
+        .login(this.user)
+        .then(account => {
+          sessionStorage.setItem('token', account.token);
+          sessionStorage.setItem('username', this.user.username);
+          this.routSwitch();
+        })
+        .catch(error => {
+          // this.error = error;
+          this.error = "业务员不存在!"
+        }); // TODO: Display error message
+    }
+  }
+  routSwitch() {
+    this.profileService
+      .getProfile(this.user.username)
+      .then(res => {
+        this.next = (res.user.is_staff) ? 'reset-pwd' : 'login';
+        let entry = (res.user.is_staff) ? 'reset-pwd' : 'login';
+        sessionStorage.setItem('entry', entry);
+        this.router.navigate([this.next]);
+      })
+      .catch(error => {
+        // this.error = error;
+        this.error = "业务员不存在!"
+      });
   }
 
   getSMS() {
@@ -36,7 +72,7 @@ export class CellPhoneResetComponent {
   onSubmit() {
     if (this.verification && this.smsCode == this.verification) {
       sessionStorage.setItem('cellPhone', this.cellPhone);
-      this.router.navigate(['dashboard/password']);
+      this.router.navigate(['reset-pwd']);
     } else {
       this.error = "验证未通过";
     }
