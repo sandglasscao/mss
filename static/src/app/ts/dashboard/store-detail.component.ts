831b050aa65f5d97302459db71b66d07d71f5b91
zhengwei2531 <1820468354@qq.com>
2017-12-07 14:36:52 +0800

    back css change; storeDetail back function add;

--- static/src/app/ts/dashboard/store-detail.component.ts
+++ static/src/app/ts/dashboard/store-detail.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from "./store";
 import {StoreService} from "./store.service";
 import "rxjs/add/operator/switchMap";
-
+import { Location }                 from '@angular/common';
 
 @Component({
   selector: 'app-teamDownLine',
@@ -12,10 +12,15 @@ import "rxjs/add/operator/switchMap";
 export class StoreDetailComponent implements OnInit {
   store: Store;
 
-  constructor(private service: StoreService) {
+  constructor(private service: StoreService,
+              private location: Location) {
   }
 
   ngOnInit(): void {
     this.store = this.service.getStore(sessionStorage.getItem('selected'));
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
