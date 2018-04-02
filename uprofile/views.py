00015a6e56e7c7275a95edb94bf0b25fa7469299
Lython7 <15210370876@163.com>
2018-04-02 15:34:09 +0800

    register check cell exist

--- uprofile/views.py
+++ uprofile/views.py
@@ -90,9 +90,10 @@ class UserRegisterApiView(APIView):
 
             except:
                 return HttpResponse(json.dumps({'cellinfo': 'cellphone_notexist'}), content_type='application/json')
+        request.data['parent_cellphone'] = '99999999999'
 
         serializer = RegisterSerializer(data=request.data)
-        request.data['parent_cellphone']='99999999999'
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
