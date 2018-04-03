c78227ad18843feb68e3c71d3e522a12b3f62d48
Lython7 <15210370876@163.com>
2018-04-03 09:49:22 +0800

    验证码逻辑修改  设置过期时间和状态

--- utility/views.py
+++ utility/views.py
@@ -275,10 +275,9 @@ class SMSClient(object):
             status = query.stutas
             resp = {}
             if status == 1:
-
                 timeflag = query.timeflag
                 timefront = request.GET.get('timeflag')
-                if timefront-timeflag<=1:# ??????
+                if timefront-timeflag<=300:# ??????
                     code = query.code
                     codefront = request.GET.get('code')
                     if code == codefront:
